bda8c0898926048071a5672f2b645509fe4da0dd
Solenel21 <solene.j.deprez@gmail.com>
2025-04-07 11:01:58 -0400

    Build docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping",
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
